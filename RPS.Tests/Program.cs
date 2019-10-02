e334f19c5a659fae8c9c549ec50c5cf6ea5021a4
Student <example@example.com>
2019-10-02 10:01:11 -0700

    add readME with Specs

---
+++ RPS.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPS.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
